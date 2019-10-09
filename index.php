11001765ad10b3bac9897e50d432cb0c2488a100
Ariqulazka <56347648+Ariqulazka@users.noreply.github.com>
2019-10-09 19:28:42 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$nama = ' Muhammad Ariqulazka ';
+$kelas = ' X RPL2 ';
+$absen = ' 16 ';
+echo 'Nama ='.$nama;
+echo '<br>';
+echo 'Kelas ='.$kelas;
+echo '<br>' ;
+echo 'Absen ='.$absen;
+?>
